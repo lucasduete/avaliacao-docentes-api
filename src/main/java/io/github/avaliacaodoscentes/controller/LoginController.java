96475b30f6bd22e5642bdbbd08b24fba5cbf0b9a
Lucas Duete <lm.duete@gmail.com>
2018-01-31 16:51:22 -0300

    Adicionando Login Controller.

---
+++ src/main/java/io/github/avaliacaodoscentes/controller/LoginController.java
@@ -0,0 +1,36 @@
+package io.github.avaliacaodoscentes.controller;
+
+import io.github.avaliacaodoscentes.model.AdministradorDAO;
+import io.github.avaliacaodoscentes.model.AlunoDAO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("login")
+public class LoginController {
+
+
+    @POST
+    @Path("loginAdmin/{email}/{senha}")
+    public Response loginAdmin(@PathParam("email") String email, @PathParam("senha") String senha) {
+
+        AdministradorDAO adminDao = new AdministradorDAO();
+
+        if (adminDao.loginAdmin(email, senha))
+            return Response.ok().build();
+        else
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+
+    @POST
+    @Path("loginAluno/{matricula}/{senha}")
+    public Response loginAluno(@PathParam("matricula") String matricula, @PathParam("senha") String senha) {
+
+        AlunoDAO alunoDao = new AlunoDAO();
+
+        if (alunoDao.loginAluno(matricula, senha))
+            return Response.ok().build();
+        else
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+}
