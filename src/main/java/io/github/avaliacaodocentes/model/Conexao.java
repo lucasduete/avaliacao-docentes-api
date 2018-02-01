dc9af534b81619d4af668af173e16efae35332ec
Lucas Duete <lm.duete@gmail.com>
2018-02-01 01:42:07 -0300

    Configurando Projeto para Deploy no Heroku.

--- src/main/java/io/github/avaliacaodocentes/model/Conexao.java
+++ src/main/java/io/github/avaliacaodocentes/model/Conexao.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    private static final String url = "jdbc:postgresql://localhost:5432/avaliacao_docente";
-    private static final String usuario = "postgres";
-    private static final String senha = "postgres";
+    private static final String url = "jdbc:postgresql://ec2-23-23-92-179.compute-1.amazonaws.com:5432/d29ih0eq9ac50k";
+    private static final String usuario = "fyrfesfmxmlamk";
+    private static final String senha = "22df713743820d272408aa6d72bee73c02f4f35f6d3e2be0223963604c66736c";
     
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
         Class.forName("org.postgresql.Driver");
