47b189eaf14063eeeacfbb80f39c10daf0619243
Murillo <murillo_eum@hotmail.com>
2018-02-01 11:39:53 -0300

    Atualizando AdministradorDAOTest e AlunoDAOTest

--- src/test/java/io/github/avaliacaodocentes/model/AlunoDAOTest.java
+++ src/test/java/io/github/avaliacaodocentes/model/AlunoDAOTest.java
@@ -6,25 +6,79 @@ import org.junit.Before;
 
 public class AlunoDAOTest {
     
-    Aluno aluno;
+    AlunoDAO alunoDao;
     
     @Before
     public void setUpBefore(){
-         aluno = new Aluno("Joao", "1234", "200010101099", "adm@email.com", 1);
+         alunoDao = new AlunoDAO();
     }
     
     @Test
-    public void testCadastrarAluno() {
-        System.out.println("cadastrarAluno");
-        AlunoDAO alunoDao = new AlunoDAO();
+    public void testCadastrarAlunoValido() {
+        //Considerando que não há um usuário ja cadastrado com a mesma matricula
+        //e exista o administrador adm@email.com e curso com código 1
+        Aluno aluno = new Aluno("Joao", "1234", "200010109901", "adm@email.com", 1);
         assertTrue(alunoDao.cadastrarAluno(aluno));
     }
     
     @Test
-    public void testLoginAluno() {
-        System.out.println("loginAluno");
-        AlunoDAO alunoDao = new AlunoDAO();
-        assertTrue(alunoDao.loginAluno(aluno.getMatricula(), aluno.getSenha()));
+    public void testCadastrarAlunoInvalidoMat() {
+        Aluno aluno = new Aluno("Maria", "1234", null, "adm@email.com", 1);
+        assertFalse(alunoDao.cadastrarAluno(aluno));
+    }
+    
+    @Test
+    public void testCadastrarAlunoInvalidoAdm() {
+        //Considerando que não existe administrador 'invalido@email.com'
+        Aluno aluno = new Aluno("Maria", "1234", "200010109902", "invalido@email.com", 1);
+        assertFalse(alunoDao.cadastrarAluno(aluno));
+    }
+    
+    @Test
+    public void testCadastrarAlunoInvalidoCurso() {
+        //Considerando que não existe curso com código -1
+        Aluno aluno = new Aluno("Carlos", "4321", "200010109903", "adm@email.com", -1);
+        assertFalse(alunoDao.cadastrarAluno(aluno));
+    }
+    
+    @Test
+    public void testBuscarValido(){
+        assertNotNull(alunoDao.buscar("200010109901"));
+    }
+    
+    @Test
+    public void testBuscarInvalidoMat(){
+        assertNull(alunoDao.buscar("null"));
+    }
+    
+    @Test
+    public void testBuscarInvalidoNull(){
+        assertNull(alunoDao.buscar(null));
+    }
+        
+    @Test
+    public void testLoginAlunoValido() {
+        try {
+            assertNotNull(alunoDao.loginAluno("200010109901","1234"));
+        } catch (Exception ex) {
+        }
+    }
+    
+    @Test
+    public void testLoginAlunoInvalidoMat() {
+        try {
+            alunoDao.loginAluno("null","1234");
+            fail("Exception deveria ser lançada");
+        } catch (Exception ex) {
+        }
+    }
+    
+    public void testLoginAlunoInvalidoSenha() {
+        try {
+            alunoDao.loginAluno("200010109901","null");
+            fail("Exception deveria ser lançada");
+        } catch (Exception ex) {
+        }
     }
     
 }
