a1326189aa578c5ecc4fdd8966f91f1283a0eb31
Murillo <murillo_eum@hotmail.com>
2018-02-01 11:39:53 -0300

    Atualizando AdministradorDAOTest e AlunoDAOTest

--- src/test/java/io/github/avaliacaodocentes/model/AdministradorDAOTest.java
+++ src/test/java/io/github/avaliacaodocentes/model/AdministradorDAOTest.java
@@ -5,26 +5,73 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AdministradorDAOTest {
-        
+    
     Administrador adm;
+    AdministradorDAO admDao;
     
     @Before
     public void setUpBefore(){
-         adm = new Administrador("adm@email.com", "Admin", "1234");
+         admDao = new AdministradorDAO();
+         adm = new Administrador ("adm@email.com", "Admin", "senha");
     }
     
     @Test
-    public void testCadastrarAdmin() {
-        System.out.println("cadastrarAdmin");
-        AdministradorDAO admDao = new AdministradorDAO();
+    public void testCadastrarAdminValido() {
         assertTrue(admDao.cadastrarAdmin(adm));
     }
     
     @Test
-    public void testLoginAdmin() {
-        System.out.println("loginAdmin");
-        AdministradorDAO admDao = new AdministradorDAO();
-        assertTrue(admDao.loginAdmin(adm.getEmail(), adm.getSenha()));
+    public void testCadastrarAdminEmailInvalido() {
+        Administrador admInv = new Administrador ("email", "Admin", "senha");
+        assertFalse(admDao.cadastrarAdmin(admInv));
+    }
+    
+    @Test
+    public void testCadastrarAdminEmailNull() {
+        Administrador admInv = new Administrador (null, "Admin", "senha");
+        assertFalse(admDao.cadastrarAdmin(admInv));
+    }
+    
+    @Test
+    public void testBuscarValido(){
+        System.out.println(adm);
+        assertNotNull(admDao.buscar(adm.getEmail()));
+    }
+    
+    @Test
+    public void testBuscarInvalido(){
+        assertNull(admDao.buscar("null"));
+    }
+    
+    @Test
+    public void testBuscarInvalidoNull(){
+        assertNull(admDao.buscar(null));
+    }
+    
+    @Test
+    public void testLoginAdminValido() {
+        try {
+            assertNotNull(admDao.loginAdmin(adm.getEmail(), adm.getSenha()));
+        } catch (Exception ex) {
+        }
+    }
+    
+    @Test
+    public void testLoginAdminEmailInvalido() {
+        try {
+            admDao.loginAdmin(null, adm.getSenha());
+            fail("Exception deveria ser lançada");
+        } catch (Exception ex) {
+        }
+    }
+    
+    @Test
+    public void testLoginAdminSenhaInvalido() {
+        try {
+            admDao.loginAdmin(adm.getEmail(), null);
+            fail("Exception deveria ser lançada");
+        } catch (Exception ex) {
+        }
     }
     
 }
