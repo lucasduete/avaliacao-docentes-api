b5be7c9c2a327cac74d1fd8112f956fc61c95e06
Lucas Duete <lm.duete@gmail.com>
2018-02-08 09:34:42 -0300

    Implementando Filtro de WebToken na aplicação.

--- src/main/java/io/github/avaliacaodocentes/controller/LoginController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/LoginController.java
@@ -6,14 +6,27 @@ import io.github.avaliacaodocentes.dao.AdministradorDAO;
 import io.github.avaliacaodocentes.model.Aluno;
 import io.github.avaliacaodocentes.dao.AlunoDAO;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.DatatypeConverter;
 import java.sql.SQLException;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Logger;
 
 @Path("login")
 public class LoginController {
 
+    private final static String SECRET_KEYTOKEN = "S3c3t-k1Y-t0%{T0K2n{4P1(GPS)}%";
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -60,4 +73,39 @@ public class LoginController {
             return Response.status(Response.Status.BAD_REQUEST).build();
         }
     }
+
+    private String gerarToken(String login,Integer limiteDias ) {
+
+        SignatureAlgorithm algoritimoAssinatura = SignatureAlgorithm.HS512;
+
+        Date agora = new Date();
+
+        Calendar expira = Calendar.getInstance();
+        expira.add(Calendar.DAY_OF_MONTH, limiteDias);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEYTOKEN);
+        SecretKeySpec key = new SecretKeySpec(apiKeySecretBytes, algoritimoAssinatura.getJcaName());
+
+        JwtBuilder construtor = Jwts.builder()
+                .setIssuedAt(agora)
+                .setIssuer(login)
+                .signWith(algoritimoAssinatura, key)
+                .setExpiration(expira.getTime());
+
+        return construtor.compact();
+    }
+
+    public Claims validaToken(String token) {
+
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEYTOKEN))
+                    .parseClaimsJws(token).getBody();
+
+            return claims;
+        } catch(Exception ex){
+            throw ex;
+        }
+    }
+
 }
---
+++ src/main/java/io/github/avaliacaodocentes/infraSecurity/FilterSecurityAuthentication.java
@@ -0,0 +1,90 @@
+package io.github.avaliacaodocentes.infraSecurity;
+
+import javax.annotation.Priority;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.Priorities;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import javax.ws.rs.ext.Provider;
+
+import io.github.avaliacaodocentes.controller.LoginController;
+import io.jsonwebtoken.Claims;
+
+import java.io.IOException;
+import java.security.Principal;
+
+@Security
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class FilterSecurityAuthentication implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            throw new NotAuthorizedException("Authorization header precisa ser provido");
+        }
+
+        String token = authorizationHeader.substring("Bearer".length()).trim();
+
+        try {
+
+            Claims claims = new LoginController().validaToken(token);
+
+            if(claims==null)
+                throw new Exception("Token inválido");
+
+            modificarRequestContext(requestContext, claims.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            requestContext.abortWith(
+                    Response.status(Response.Status.UNAUTHORIZED).build());
+        }
+    }
+
+    private void modificarRequestContext(ContainerRequestContext requestContext, String indentificador) {
+
+        final SecurityContext currentSecurityContext = requestContext.getSecurityContext();
+
+        requestContext.setSecurityContext(new SecurityContext() {
+            @Override
+            public Principal getUserPrincipal() {
+                return new Principal() {
+
+                    @Override
+                    public String getName() {
+                        return indentificador;
+                    }
+                };
+            }
+
+            @Override
+            public boolean isUserInRole(String role) {
+                return true;
+            }
+
+            @Override
+            public boolean isSecure() {
+                return currentSecurityContext.isSecure();
+            }
+
+            @Override
+            public String getAuthenticationScheme() {
+                return "Bearer";
+            }
+
+        });
+    }
+
+    public static String getToken(ContainerRequestContext requestContext) {
+        return requestContext
+                .getSecurityContext()
+                .getUserPrincipal()
+                .getName();
+    }
+}
