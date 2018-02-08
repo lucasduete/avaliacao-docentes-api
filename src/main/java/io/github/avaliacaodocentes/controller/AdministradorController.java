315c14f04bc48eaa91143bcb28e1e450b6e42625
Lucas Duete <lm.duete@gmail.com>
2018-02-08 10:31:19 -0300

    Refatorando AdminController para que Adicionar Filtro de Acesso aos Métodos, Removendo Metodos não pertencentes ao Alunos de AlunoController.

--- src/main/java/io/github/avaliacaodocentes/controller/AdministradorController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/AdministradorController.java
@@ -1,7 +1,11 @@
 package io.github.avaliacaodocentes.controller;
 
+import io.github.avaliacaodocentes.dao.AlunoDAO;
+import io.github.avaliacaodocentes.infraSecurity.Security;
+import io.github.avaliacaodocentes.infraSecurity.model.NivelAcesso;
 import io.github.avaliacaodocentes.model.Administrador;
 import io.github.avaliacaodocentes.dao.AdministradorDAO;
+import io.github.avaliacaodocentes.model.Aluno;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -12,6 +16,7 @@ import javax.ws.rs.core.Response;
 @Path("admin")
 public class AdministradorController {
 
+    @Security(NivelAcesso.NIVEL_1)
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("cadastrarAdmin/")
@@ -20,6 +25,21 @@ public class AdministradorController {
         AdministradorDAO adminDao = new AdministradorDAO();
 
         if (adminDao.cadastrarAdmin(admin))
+            return Response.ok().build();
+        else
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+    }
+
+    @Security(NivelAcesso.NIVEL_1)
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("cadastrarAluno/")
+    public Response cadastrarAdmin(Aluno aluno) {
+
+        AlunoDAO alunoDao = new AlunoDAO();
+
+        if (alunoDao.cadastrarAluno(aluno))
             return Response.ok().build();
         else
             return Response.status(Response.Status.BAD_REQUEST).build();
--- src/main/java/io/github/avaliacaodocentes/controller/AlunoController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/AlunoController.java
@@ -1,28 +1,9 @@
 package io.github.avaliacaodocentes.controller;
 
-import io.github.avaliacaodocentes.model.Aluno;
-import io.github.avaliacaodocentes.dao.AlunoDAO;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 @Path("aluno")
 public class AlunoController {
 
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Path("cadastrarAluno/")
-    public Response cadastrarAdmin(Aluno aluno) {
 
-        AlunoDAO alunoDao = new AlunoDAO();
-
-        if (alunoDao.cadastrarAluno(aluno))
-            return Response.ok().build();
-        else
-            return Response.status(Response.Status.BAD_REQUEST).build();
-
-    }
 }
