1fd537cd5a8e5fc5e018f23b3ed74f3a7b607250
Lucas Duete <lm.duete@gmail.com>
2018-02-10 14:38:42 -0300

    Implementando Classe de Modelo de Professor e Método de Cadastro em ProfessorDao.java

---
+++ src/main/java/io/github/avaliacaodocentes/dao/ProfessorDao.java
@@ -0,0 +1,49 @@
+package io.github.avaliacaodocentes.dao;
+
+import io.github.avaliacaodocentes.factory.Conexao;
+import io.github.avaliacaodocentes.model.Professor;
+import io.github.avaliacaodocentes.resources.Encryption;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProfessorDao {
+
+    private Connection conn;
+
+    public ProfessorDao() {
+        try {
+            conn = Conexao.getConnection();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean cadastrar(Professor professor) {
+
+        String sql = "INSERT INTO Professor(Nome, Senha, Matricula, Nota) " +
+                "VALUES (?,?,?,?)";
+
+        try {
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, professor.getNome());
+            stmt.setString(2, Encryption.encrypt(professor.getSenha()));
+            stmt.setString(3, professor.getMatricula());
+            stmt.setFloat(4, professor.getNota());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/io/github/avaliacaodocentes/model/Professor.java
@@ -0,0 +1,81 @@
+package io.github.avaliacaodocentes.model;
+
+import java.util.Objects;
+
+public class Professor {
+
+    private String nome;
+    private String senha;
+    private String matricula;
+    private float nota;
+
+    public Professor() {
+
+    }
+
+    public Professor(String nome, String senha, String matricula, float nota) {
+        this.nome = nome;
+        this.senha = senha;
+        this.matricula = matricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public float getNota() {
+        return nota;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Professor professor = (Professor) o;
+        return nota == professor.nota &&
+                Objects.equals(nome, professor.nome) &&
+                Objects.equals(senha, professor.senha) &&
+                Objects.equals(matricula, professor.matricula);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(nome, senha, matricula, nota);
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", nota=" + nota +
+                '}';
+    }
+}
