bf3c68fdd51699d7afd0c84a427d98918c7dea4d
Lucas Duete <lm.duete@gmail.com>
2018-02-10 14:03:10 -0300

    Implementando Classe de Modelo de Curso e Metodo de Cadastro de Cursos em CursoDao.java

---
+++ src/main/java/io/github/avaliacaodocentes/dao/CursoDao.java
@@ -0,0 +1,46 @@
+package io.github.avaliacaodocentes.dao;
+
+import io.github.avaliacaodocentes.factory.Conexao;
+import io.github.avaliacaodocentes.model.Curso;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CursoDao {
+
+    private Connection conn;
+
+    public CursoDao () {
+        try {
+            conn = Conexao.getConnection();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean cadastrar(Curso curso) {
+
+        String sql = "INSERT INTO Curso(Nome, Codigo, EmailAdministrador) VALUES (?,?,?);";
+
+        try {
+
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1, curso.getNome());
+            stmt.setInt(2, curso.getCodigo());
+            stmt.setString(3, curso.getEmailAdministrador());
+
+            stmt.executeUpdate();
+
+            stmt.close();
+            conn.close();
+        } catch (SQLException ex) {
+
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/io/github/avaliacaodocentes/model/Curso.java
@@ -0,0 +1,69 @@
+package io.github.avaliacaodocentes.model;
+
+import java.util.Objects;
+
+public class Curso {
+
+    private String nome;
+    private int codigo;
+    private String emailAdministrador;
+
+    public Curso() {
+
+    }
+
+    public Curso(String nome, int codigo, String emailAdministrador) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.emailAdministrador = emailAdministrador;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEmailAdministrador() {
+        return emailAdministrador;
+    }
+
+    public void setEmailAdministrador(String emailAdministrador) {
+        this.emailAdministrador = emailAdministrador;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return codigo == curso.codigo &&
+                Objects.equals(nome, curso.nome) &&
+                Objects.equals(emailAdministrador, curso.emailAdministrador);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(nome, codigo, emailAdministrador);
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "nome='" + nome + '\'' +
+                ", codigo=" + codigo +
+                ", emailAdministrador='" + emailAdministrador + '\'' +
+                '}';
+    }
+}
