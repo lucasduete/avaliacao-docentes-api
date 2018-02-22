3390f5167894975e07fecff3a370eeb3847c5305
Lucas Duete <lm.duete@gmail.com>
2018-02-22 13:50:54 -0300

    Adicionando Método que Aluno Pode Enviar uma Avaliação

--- src/main/java/io/github/avaliacaodocentes/controller/AlunoController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/AlunoController.java
@@ -1,10 +1,12 @@
 package io.github.avaliacaodocentes.controller;
 
 import io.github.avaliacaodocentes.dao.AlunoDao;
+import io.github.avaliacaodocentes.dao.AvaliacaoDao;
 import io.github.avaliacaodocentes.infraSecurity.Security;
 import io.github.avaliacaodocentes.infraSecurity.TokenManagement;
 import io.github.avaliacaodocentes.infraSecurity.model.NivelAcesso;
 import io.github.avaliacaodocentes.model.Aluno;
+import io.github.avaliacaodocentes.model.Avaliacao;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -14,6 +16,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 @Path("aluno")
 public class AlunoController {
@@ -44,6 +47,35 @@ public class AlunoController {
             ex.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+    }
 
+    @Security(NivelAcesso.NIVEL_2)
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("enviarAvaliacao/")
+    public Response enviarAvaliacao(Avaliacao avaliacao,
+                                    @Context SecurityContext securityContext) {
+
+        if ((avaliacao.getMatProfessor() == null || avaliacao.getMatProfessor().isEmpty())
+            || (avaliacao.getPontuacoes() == null || avaliacao.getPontuacoes().isEmpty()))
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        try {
+
+            AvaliacaoDao avaliacaoDao = new AvaliacaoDao();
+
+            avaliacao.setData(LocalDate.now());
+            avaliacao.setMatAluno(
+                    TokenManagement.getToken(securityContext)
+            );
+
+            avaliacaoDao.cadastrar(avaliacao);
+
+            return Response.status(Response.Status.OK).build();
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
