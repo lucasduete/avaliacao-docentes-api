02bde10f7e01ea34ccd99152b3885afc34011318
Lucas Duete <lm.duete@gmail.com>
2018-02-27 01:04:40 -0300

    Implementando Metodo de Gerar Ranking Semestral em RankingController.

--- src/main/java/io/github/avaliacaodocentes/controller/RankingController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/RankingController.java
@@ -7,6 +7,7 @@ import io.github.avaliacaodocentes.model.Professor;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,6 +28,30 @@ public class RankingController {
                                                         .criarRankingDao();
 
             List<Professor> ranking = rankingDao.gerarRanking();
+
+            return Response.ok(ranking).build();
+
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
+    @Security
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("rankingSemestral/{semestre}")
+    public Response getRankingSemestral(@PathParam("semestre") String semestre) {
+
+        if (semestre == null || semestre.isEmpty() || semestre.length() != 6)
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        try {
+            RankingDaoInterface rankingDao = Fabrica.criarFabricaDaoPostgres()
+                    .criarRankingDao();
+
+            List<Professor> ranking = rankingDao.gerarRankingSemestral(semestre);
 
             return Response.ok(ranking).build();
 
