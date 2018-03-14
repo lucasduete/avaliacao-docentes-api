417b17789e52daf6daf1412575fde4a1d7b7ec9f
Lucas Duete <lm.duete@gmail.com>
2018-03-14 14:57:44 -0300

    Implementando Método que Setta a Foto de um Professor.

--- src/main/java/io/github/avaliacaodocentes/controller/ProfessorController.java
+++ src/main/java/io/github/avaliacaodocentes/controller/ProfessorController.java
@@ -2,12 +2,15 @@ package io.github.avaliacaodocentes.controller;
 
 import io.github.avaliacaodocentes.dao.interfaces.ProfessorDaoInterface;
 import io.github.avaliacaodocentes.factory.Fabrica;
+import io.github.avaliacaodocentes.infraSecurity.Security;
+import io.github.avaliacaodocentes.infraSecurity.model.NivelAcesso;
+import io.github.avaliacaodocentes.resources.FotoManagement;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
 
 @Path("professor")
@@ -27,7 +30,31 @@ public class ProfessorController {
             ex.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+    }
 
+    @PUT
+    @Security(NivelAcesso.NIVEL_1)
+    @Consumes("image/jpeg")
+    @Path("foto/{matProfessor}/")
+    public Response setFotoProfessor(File foto,
+                                     @PathParam("matProfessor") String matProfessor) {
 
+        if (foto == null)
+            return Response.status(Response.Status.BAD_REQUEST).build();
+
+        try {
+            ProfessorDaoInterface professorDao = Fabrica.criarFabricaDaoPostgres().criarProfessorDao();
+
+            String fotoBase64 = FotoManagement.encodeFoto(foto);
+
+            if (professorDao.atualizarFoto(fotoBase64, matProfessor))
+                return Response.status(Response.Status.OK).build();
+            else
+                return Response.status(Response.Status.BAD_REQUEST).build();
+
+        } catch (SQLException | ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
